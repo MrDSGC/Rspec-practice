046e7fb3709d0a686f8204c06657082abc93a9a7
MrDSGC <dcheng47@gmail.com>
2017-05-04 17:14:34 -0700

    model methods/ instance methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    before(:each) do
-      User.create!(username: 'daniel C', password: 'password')
-    end
 
-
+    let (:user) { User.create!(username: 'Donnie', password: 'password')}
     it { should validate_presence_of(:username)}
     it { should validate_presence_of(:session_token)}
     it { should validate_presence_of(:password_digest)}
@@ -17,4 +14,36 @@ RSpec.describe User, type: :model do
     it { should have_many(:favorites)}
     it { should have_many(:favorite_benches)}
   end
+
+  describe 'instance_methods' do
+    describe '#reset_session_token!' do
+      let (:user) { User.create!(username: 'bob', password: 'password')}
+      it 'resets the session token' do
+        # test goes here
+        old_session_token = user.session_token
+        user.reset_session_token!
+        expect(user.session_token).not_to eql(old_session_token)
+      end
+    end
+  end
+
+  describe 'model_methods' do
+    describe '.find_by_credentials' do
+      context 'when given correct credentials' do
+        let! (:user) { User.create!(username: 'bob', password: 'password')}
+        it 'should find the right user' do
+          found = User.find_by_credentials('bob', 'password')
+          expect(found).to eql(user)
+        end
+      end
+
+      context 'when given incorrect credentials' do
+        it 'should return nil' do
+          found = User.find_by_credentials("hi", 'asdf')
+          expect(found).to eql(nil)
+        end
+      end
+    end
+  end
+
 end
